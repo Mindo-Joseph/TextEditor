44f72ef32f83982cfa081d2e08e0a7c3d6f99f30
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-02-03 06:31:20 +0300

    Add font style component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const optionsArea = document.createElement('div');
+optionsArea.className = 'options-area';
 const editorArea = document.createElement('blockquote');
 const main = document.getElementById('main');
 editorArea.className = 'Editor';
@@ -16,7 +18,27 @@ boldIcon.className = 'fas fa-bold';
 const textColorIcon = document.createElement('i');
 textColorIcon.className = 'fas fa-paint-roller';
 const fontChangeIcon = document.createElement('i');
+const icon = document.createElement('i');
 fontChangeIcon.className = 'fas fa-font';
+fontChangeIcon.appendChild(icon);
+fontChangeIcon.addEventListener('click', function () {
+    let select = document.createElement('select');
+    select.id = 'font-selection';
+    let option1 = document.createElement('option');
+    option1.value = 'Arial';
+    option1.text = 'Arial';
+    let option2 = document.createElement('option');
+    option2.value = 'Sans Serif';
+    option2.text = 'Sans Serif';
+    let option3 = document.createElement('option');
+    option3.value = 'Verdana';
+    option3.text = 'Verdana';
+    select.add(option1);
+    select.add(option2);    
+    select.add(option2);
+    optionsArea.appendChild(select);
+    select.onchange = console.log(select.options[select.selectedIndex].text);
+});
 const italicIcon = document.createElement('i');
 italicIcon.className = 'fas fa-italic';
 const underlineIcon = document.createElement('i');
@@ -44,6 +66,35 @@ alignTools.appendChild(leftAlignIcon);
 alignTools.appendChild(centerAlignIcon);
 alignTools.appendChild(rightAlignIcon);
 toolbar.appendChild(alignTools);
-
 main.appendChild(toolbar);
+main.appendChild(optionsArea);
 main.appendChild(editorArea)
+
+
+let getSelectedText = () => {
+    let text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+       
+
+    } else if (document.selection && document.selection.type !== "Control") {
+        text = document.selection.createRange().text;
+
+    }
+    return text;
+}
+let changeFont = (font) => {
+    let selectedText = window.getSelection();
+    if (selectedText.rangeCount) {
+        let element = document.createElement('span');
+        element.style = `font-family: ${font.value};`;
+        element.innerHTML = selectedText.toString();
+
+        let range = selectedText.getRangeAt(0);
+        range.deleteContents();
+        range.insertNode(element);
+    }
+
+
+}
+document.addEventListener('mouseup',getSelectedText)
